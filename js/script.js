76ae5055fba65c721d1fbfaade2b7130b0f7ba13
Rani Dardur <dardurranisc@gmail.com>
2024-10-27 15:59:35 +0300

    second commit

--- js/script.js
+++ js/script.js
@@ -1,24 +1,24 @@
 /*Фиксированное меню HEADER */
-let header = document.getElementById('header').classList
-let active__class_position = "header_position"
+const headerClassList = document.getElementById('header').classList
+const activeClassPosition = "header_position"
 
-let header__inner = document.getElementById('header__inner').classList
-let active__class_inner = "header__inner-fixed"
+const headerInner = document.getElementById('header__inner').classList
+const activeClassInner = "header__inner-fixed"
 
-let header__logo = document.getElementById('header__logo').classList
-let active__class_logo = "header__logo-fixed"
+const headerLogo = document.getElementById('header__logo').classList
+const activeClassLogo = "header__logo-fixed"
 
 
 window.addEventListener('scroll', e => {
   if(pageYOffset > 450){
-    header.add(active__class_position)
-    header__inner.add(active__class_inner)
-    header__logo.add(active__class_logo)
+    headerClassList.add(activeClassPosition)
+    headerInner.add(activeClassInner)
+    headerLogo.add(activeClassLogo)
   } 
   else {
-      header.remove(active__class_position)
-      header__inner.remove(active__class_inner)
-      header__logo.remove(active__class_logo)
+      headerClassList.remove(activeClassPosition)
+      headerInner.remove(activeClassInner)
+      headerLogo.remove(activeClassLogo)
   }
 })
 function Hidden(event) {
